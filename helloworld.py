3a808d45bbf87790e8f9b87eff8ab46e7d6b89eb
gozld5153 <gozld5153@naver.com>
2021-03-31 20:59:31 +0900

    test_phython

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+# url = "http://naver.com"
+url = "https://google.co.kr"
+a = url.index('/')
+a1 = url.index("/", a + 1)
+text = url[a1 + 1:url.index('.')]
+password = text[:3] + str(len(text)) + str(text.count("e")) + "!"
+print("{0} 사이트의 비밀번호는 {1} 입니다." .format(url, password))
---
+++ test.py
@@ -0,0 +1,10 @@
+from random import *
+print(random())
+print(random() * 10)
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10) + 1)
+
+print(int(random()*45)+1)
+print(randrange(1, 46))
