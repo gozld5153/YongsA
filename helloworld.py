48047dfadb7aba67c91a6e9f677d5c490d4b63e0
gozld5153 <gozld5153@naver.com>
2021-03-31 21:05:39 +0900

    test_Python

--- helloworld.py
+++ helloworld.py
@@ -5,3 +5,5 @@ a1 = url.index("/", a + 1)
 text = url[a1 + 1:url.index('.')]
 password = text[:3] + str(len(text)) + str(text.count("e")) + "!"
 print("{0} 사이트의 비밀번호는 {1} 입니다." .format(url, password))
+
+# 1:22:36 5-1리스트부터
