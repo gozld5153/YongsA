f0114d3c229b420d53602364c2db41f8b46f81fd
gozld5153 <81503846+gozld5153@users.noreply.github.com>
2021-04-30 02:39:10 +0900

    home

---
+++ main.js
@@ -0,0 +1,24 @@
+const letter = document.querySelector('.yongsa');
+const yongsa = letter.textContent;
+const array = yongsa.split("");
+console.log(array);
+
+letter.innerHTML = "";
+
+for( let i=0; i < array.length; i++){
+    letter.innerHTML += "<span>" + array[i] + "</span>"
+}
+
+let char = 0;
+let timer = setInterval(onTick, 300);
+
+function onTick(){
+    const span = letter.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char == array.length){
+      document.querySelector('.home').classList.add('fade2');
+      clearInterval(timer);
+      timer = null;
+    }
+}
